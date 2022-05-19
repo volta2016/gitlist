64420cbc3f0e5a67ad456ee523df5c788547d8a1
volta2016 <giovoltadesign@gmail.com>
2022-05-19 17:43:09 -0400

    [gitlist] service

---
+++ src/hook/useFetchGihub.js
@@ -0,0 +1,10 @@
+import { useState, useEffect } from "react";
+
+const useFetchGithub = () => {
+  const [search, setSearch] = useState({});
+  const [repoList, setRepoList] = useState([]);
+
+  useEffect(() => {}, [query]);
+};
+
+export default useFetchGithub;
